a807cc01da040334c4ec9534a904ef8d3167294a
dpolishchuk <d.polishchuk@incuube.com>
2018-08-14 11:36:36 +0300

    delete in-memory

--- src/main/java/test/service/MainService.java
+++ src/main/java/test/service/MainService.java
@@ -6,10 +6,8 @@ import test.domain.MyRequestEntity;
 import test.repository.RequestsRepository;
 
 import java.time.LocalDateTime;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @Service
 public class MainService {
@@ -17,33 +15,29 @@ public class MainService {
     @Autowired
     private RequestsRepository repository;
 
-    private LinkedList<MyRequestEntity> requests = new LinkedList<>();
-
-
     public void add(String method, String body, Map<String, String> headers, Map<String, String> params) {
         MyRequestEntity request = new MyRequestEntity(method, body, headers, params);
         repository.save(request);
-        requests.addFirst(request);
         removeOld();
     }
 
     private void removeOld() {
         repository.removeAllByTimeBefore(LocalDateTime.now().minusDays(1));
+        long count = repository.count();
+        if (count > 20) {
+            repository.removeAllByTimeBefore(LocalDateTime.now().minusMinutes(5));
+        }
     }
 
     public List<MyRequestEntity> getAllRequests() {
         return repository.findAll();
-//        return requests;
     }
 
     public MyRequestEntity getRequest(String id) {
-        Optional<MyRequestEntity> first = requests.stream().filter(r -> r.get_id().equals(id)).findFirst();
-//        return first.orElse(null);
         return repository.findById(id).orElse(null);
     }
 
     public void removeAll() {
-        requests.clear();
         repository.deleteAll();
     }
 }
